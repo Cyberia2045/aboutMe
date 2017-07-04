adb016e04f501950736b6f8eeb489ad4375034fd
Cyberia 2045 <sagansociety86@gmail.com>
2017-07-03 23:59:22 -0400

    The portfolio now links back and forth between intro and project view

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ var contentContainer = document.getElementsByClassName("content-container")[0];
 
 var projects = document.getElementsByClassName("project-container")[0];
 
+var homeButton = document.getElementsByClassName("home-button")[0];
+
 projectButton.addEventListener("click", projectLoader);
+
+homeButton.addEventListener("click", home);
 
 function projectLoader() {
 	contentContainer.style.display = "none";
@@ -13,3 +17,21 @@ function projectLoader() {
 	projects.style.animation = "welcome 6s ease-in";
 	projects.style.animationFillMode = "forwards";
 }
+
+function home() {
+	projects.style.display = "none";
+	projectButton.style.display = "block";
+	contentContainer.style.display = "flex";
+	contentContainer.style.animation = "welcome 6s ease-in";
+	contentContainer.style.animationFillMode = "forwards";
+}
+
+
+
+
+
+
+
+
+
+
